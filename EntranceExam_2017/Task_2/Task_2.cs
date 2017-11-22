114d14acb36fed45b715de5193017f821c6ad62b
Unknown <djnoreaga@gmail.com>
2017-11-22 12:22:08 +0200

    just beginning

---
+++ EntranceExam_2017/Task_2/Task_2.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class Task_2
+    {
+        static void Main()
+        {
+            int b1 = int.Parse(Console.ReadLine());
+            int b2 = int.Parse(Console.ReadLine());
+            int b3 = int.Parse(Console.ReadLine());
+            int l = int.Parse(Console.ReadLine());
+
+            int ll = 0;
+            for (int i = 1; i <= l; i++)
+            {
+                ll += i;
+            }
+
+            long[] b = new long[ll];
+            b[0] = b1;
+            b[1] = b2;
+            b[2] = b3;
+
+            for (int i = 3; i < ll; i++)
+            {
+                b[i] = b[i - 1] + b[i - 2] + b[i - 3];
+            }
+
+            int counter = 0;
+            for (int i = 1; i <= l; i++)
+            {
+                for (int k = 1; k <= i; k++)
+                {
+                    if (k < i)
+                    {
+                        Console.Write("{0} ", b[counter]);
+                        counter++;
+                    }
+                    else
+                    {
+                        Console.Write("{0}", b[counter]);
+                        counter++;
+                    }
+
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ EntranceExam_2017/Task_3/Task_3.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    class Task_3
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+
+            long[] arr1 = input.Split(' ').Select(str => long.Parse(str)).ToArray();
+
+            BigInteger[] arr2 = new BigInteger[arr1.Length - 1];
+
+            for (int i = 0; i < arr1.Length - 1; i++)
+            {
+                if (arr1[i + 1] >= arr1[i])
+                {
+                    arr2[i] = arr1[i + 1] - arr1[i];
+                }
+                else
+                {
+                    arr2[i] = arr1[i] - arr1[i + 1];
+
+                }
+
+            }
+
+            for (int i = 0; i < arr2.Length - 1; i++)
+            {
+                if (arr2[i] % 2 == 0 && arr2[i] != 0)
+                {
+                    arr2[i + 1] = 0;
+                }
+            }
+            BigInteger sum = 0;
+
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                if (arr2[i] % 2 == 0)
+                {
+                    sum += arr2[i];
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
