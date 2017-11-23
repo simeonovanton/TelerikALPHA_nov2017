178f196387076c725235c4037567a7bf7268c466
simeonovanton <djnoreaga@gmail.com>
2017-11-23 07:05:20 +0200

    Resolving some entrance exam tasks

---
+++ EntranceExam_2017/Task_13_1/Task_13_1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_13_1
+{
+    class Task_13_1
+    {
+        static void Main()
+        {
+            int year = int.Parse(Console.ReadLine());
+            int month = int.Parse(Console.ReadLine());
+            int day = int.Parse(Console.ReadLine());
+            
+            string[] months = new string[] {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+
+            month -= 1;
+            day -= 1;
+            if (day == 0)
+            {
+                day = 31;
+            }
+
+            Console.WriteLine("{0}-{1}-{2}", day, months[month], year);
+
+        }
+    }
+}
