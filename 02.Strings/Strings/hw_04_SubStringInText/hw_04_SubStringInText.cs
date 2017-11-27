885d217bef604ddf47c9e33ad9b3beb4a7b2c04d
simeonovanton <djnoreaga@gmail.com>
2017-11-27 23:48:30 +0200

    hw_04 SubString In text - Done!

---
+++ 02.Strings/Strings/hw_04_SubStringInText/hw_04_SubStringInText.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw_04_SubStringInText
+{
+    class hw_04_SubStringInText
+    {
+        static void Main()
+        {
+            string searchText = Console.ReadLine().ToLower();
+            string input = Console.ReadLine().ToLower();
+          
+            int counter = 0;
+
+            for (int i = 0; i <= input.Length - searchText.Length; i++)
+            {
+                if (searchText == input.Substring(i, searchText.Length))
+                {
+                    counter++;
+                }
+            }
+           
+            Console.WriteLine(counter);
+        }
+    }
+}
