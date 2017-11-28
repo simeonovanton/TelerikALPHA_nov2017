9ac159a499b06be02aa4f04abef9fa08a2a062b0
Anton Simeonov <djnoreaga@gmail.com>
2017-11-28 12:47:27 +0200

    hw_03 Parse Tags 15/100

---
+++ 02.Strings/Strings/hw_05_ParseTags/hw_05_ParseTags.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class hw_05_ParseTags
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        string openTag = "<upcase>";
+        string closeTag = "</upcase>";
+        StringBuilder output = new StringBuilder();
+        bool toUpper = false;
+        int index = 0;
+
+        //while ((index + openTag.Length) < input.Length && (index + closeTag.Length) < input.Length)
+        while (index < input.Length)
+        {
+            if ((index + openTag.Length) < input.Length )
+            {
+                if (input.Substring(index, openTag.Length) == openTag)
+                {
+                    toUpper = true;
+                    index += openTag.Length;
+                }
+            }
+
+            if ((index + closeTag.Length) < input.Length)
+            {
+                if (input.Substring(index, closeTag.Length) == closeTag)
+                {
+                    toUpper = false;
+                    index += closeTag.Length;
+                }
+            }
+           
+
+            if (toUpper)
+            {
+                output.Append(Char.ToUpper(input[index]));
+            }
+            else
+            {
+                output.Append(input[index]);
+            }
+
+            index++;
+        }
+        Console.WriteLine(output);
+
+    }
+}
+
