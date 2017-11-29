05c2fbf3dfacda98087d1c5c989b602437512967
simeonovanton <djnoreaga@gmail.com>
2017-11-29 23:05:29 +0200

    hw ... to hw_10 - Done!

---
+++ 03.Methods/hw_09_SortingArray/SortingArray.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class SortingArray
+{
+    static int[] SortArray_Ascending( int[] array)
+    {
+        int buffer = 0;
+        for (int i = array.Length - 1; i > 0; i--)
+        {
+            for (int j = i - 1; j >= 0; j--)
+            {
+                if (array[i] < array[j])
+                {
+                    buffer = array[j];
+                    array[j] = array[i];
+                    array[i] = buffer;
+                }
+            }
+        }
+        return array;
+    }
+
+    static string IntArrayToString(int[] array)
+    {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < array.Length; i++)
+        {
+            sb.Append(array[i]);
+            if (i == array.Length - 1)
+            {
+                break;
+            }
+            sb.Append(' ');
+        }
+        return sb.ToString();
+    }
+
+    static void Main()
+    {
+        Console.ReadLine(); //Unnecessary
+
+        int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+        int[] sortedArray = SortArray_Ascending(arr);
+
+        Console.WriteLine(IntArrayToString(sortedArray));
+    }
+}
+
---
+++ 03.Methods/hw_10_nFactorial/nFactorial.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+
+class nFactorial
+{
+    static BigInteger CalculateFactorial(int number)
+    {
+        BigInteger result = 1; ;
+        for (int i = 1; i <= number; i++)
+        {
+            result *= i;
+        }
+        return result;
+    }
+
+    static void Main()
+    {
+        int numberN = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(CalculateFactorial(numberN));
+    }
+}
+
