90e2843eec441872bc937d9cb3128e980eb615c3
Anton Simeonov <djnoreaga@gmail.com>
2017-11-30 14:44:35 +0200

    hw_08_ExtractSentence - TO DO!

---
+++ 02.Strings/Strings/hw_06_StringLength/StringLength.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class StringLength
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        StringBuilder sb = new StringBuilder();
+        sb.Append(input);
+        if (input.Length < 20)
+        {
+            for (int i = 20 - input.Length; i > 0 ; i--)
+            {
+                sb.Append('*');
+            }
+        }
+        Console.WriteLine(sb.ToString());
+    }
+}
+
---
+++ 02.Strings/Strings/hw_08_ExtractSentenses/ExtractSentenses.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class ExtractSentenses
+{
+    static void Main()
+    {
+        string word = Console.ReadLine();
+        string str = Console.ReadLine();
+        //string str = "0.23.567.9";
+
+        int indexWord = 0;
+        int indexDotFirst = 0;
+        int indexDotLast = 0;
+
+        while (true)
+        {
+            if (indexWord == -1)
+            {
+                break;
+            }
+
+            indexWord = str.IndexOf(word, indexWord + 1);
+
+            if (indexWord == -1)
+            {
+                break;
+            }
+
+            indexDotFirst = str.LastIndexOf('.', indexWord);
+            if (indexDotFirst == -1)
+            {
+                indexDotFirst = 0;
+            }
+
+            indexDotLast = str.IndexOf('.', indexWord);
+
+            int sentenceLength = indexDotLast - indexDotFirst;
+            if (isLetter(str[indexWord - 1]))
+            {
+                Console.WriteLine(str.Substring(indexDotFirst + 1, sentenceLength));
+
+            }
+        }
+    }
+}
