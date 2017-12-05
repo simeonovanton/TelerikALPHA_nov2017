a391e176a67959b793028dff63f48382722b1249
Anton Simeonov <djnoreaga@gmail.com>
2017-12-05 14:51:30 +0200

    Task_1 100% Razgele! :(

--- Exam_1_CSharp_Advanced/Task_1/Task_1.cs
+++ Exam_1_CSharp_Advanced/Task_1/Task_1.cs
@@ -10,68 +10,42 @@ class Task_1
 {
     static void Main()
     {
-        string inputStr = Console.ReadLine();
-        //BigInteger input1 = BigInteger.Parse(Console.ReadLine());
-        //List<int> input2 = new List<int>();
-        //if (input1 < 0)
-        //{
-        //    input1 *= -1;
-        //}
-        //
-        //do
-        //{
-        //    input2.Add((int)input1 % 10);
-        //    input1 = input1 / 10;
-        //} while (input1 > 0);
-
-        //int[] input = new int[input2.Count];
-        //for (int i = input2.Count - 1; i >= 0; i--)
-        //{
-        //    input[input2.Count - i - 1] = input2[i];
-        //}
-       //int[] input = new int[inputStr.Length];
-       //for (int i = 0; i < inputStr.Length; i++)
-       //{
-       //    input[i] = Math.Abs((inputStr[i] - '0'));
-       //}
-
-        int[] result = new int[input.Length];
-        int index = 0;
-
-        for (int i = result.Length - 1; i >= 0 ; i--)
+        string input = Console.ReadLine();
+        long result = 0;
+        
+        for (int i = input.Length - 1, index = 1; i >= 0 ; i--, index ++)
         {
-            index = input.Length - i;
-
+            if (input[i] == '-')
+            {
+                continue;
+            }
             if (index % 2 == 0)
             {
-                result[i] = input[i] * input[i] * index;
+                result += (input[i] - '0') * (input[i] - '0') * index;
             }
             else
             {
-                result[i] = input[i] * index * index;
+                result += (input[i] - '0') * index * index;
             }
         }
-        BigInteger resultResult = 0;
-        foreach (var item in result)
-        {
-            resultResult += item;
-        }
-        // Console.WriteLine(resultResult);
+      
+      
+        // Console.WriteLine(result);
       
         int lengthMessage = 0;
         int s = 0;
         int start = 0;
 
-        if (resultResult % 10 == 0)
+        if (result % 10 == 0)
         {
-            Console.WriteLine(resultResult);
+            Console.WriteLine(result);
             Console.WriteLine("Big Vik wins again!");
             return;
         }
         else
         {
-            lengthMessage = (int)resultResult % 10;
-            s = (int)resultResult % 26;
+            lengthMessage = (int)result % 10;
+            s = (int)result % 26;
             start = s;
             string alphabet = "abcdefghijklmnopqrstuvwxyz";
             StringBuilder sb = new StringBuilder();
@@ -82,7 +56,7 @@ class Task_1
                 int position = (start + i) % 26;
                 sb.Append(alphabet[position]);
             }
-            Console.WriteLine(resultResult);
+            Console.WriteLine(result);
             Console.WriteLine(sb.ToString().ToUpper());
             return;
         }
---
+++ Exam_1_CSharp_Advanced/Task_1_new_solution/Task_1_new_solution.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Task_1_new_solution
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        int inputLength = input.Length;
+        long result = 0;
+        
+        
+        if (input[0] == '-')
+        {
+            for (int i = inputLength - 1,  index = 1; i > 0; i--, index++)
+            {
+                if (index % 2 == 0)
+                {
+                    result += index * (input[i] - 48) * (input[i] - 48);
+                }
+                else
+                {
+                    result += index * index * (input[i] - 48);
+                }
+            }
+        }
+        else
+        {
+            for (int i = inputLength - 1, index = 1; i >= 0; i--, index++)
+            {
+                if (index % 2 == 0)
+                {
+                    result += index * (input[i] - 48) * (input[i] - 48);
+                }
+                else
+                {
+                    result += index * index * (input[i] - 48);
+                }
+            }
+        }
+        
+
+        //long result = 0;
+        //
+        //
+        //for (int i = 0, index = input.Length; i < input.Length; i++, index--)
+        //{
+        //    string curr = input[i].ToString();
+        //    if (curr == "-")
+        //    {
+        //        continue;
+        //    }
+        //    var digit = int.Parse(curr);
+        //
+        //    if (index % 2 == 0) // even
+        //    {
+        //        result += (long)Math.Pow(digit, 2) * index;
+        //    }
+        //    else // odd
+        //    {
+        //        result += digit * ((long)Math.Pow(index, 2));
+        //    }
+        //}
+
+        if (result % 10 == 0)
+        {
+            Console.WriteLine(result);
+            Console.WriteLine("Big Vik wins again!");
+            return;
+        }
+        else
+        {
+            int s = (int)result % 26;
+            int start = s;
+            //start = s;
+            string alphabet = "abcdefghijklmnopqrstuvwxyz";
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < (int)result % 10; i++)
+            {
+                // int position = (start + i - 1) % 26;
+                int position = (start + i) % 26;
+                sb.Append(alphabet[position]);
+            }
+            Console.WriteLine(result);
+            Console.WriteLine(sb.ToString().ToUpper());
+            return;
+
+        }
+
+        //Console.WriteLine(result);
+    }
+}
+
