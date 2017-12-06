30eac486a1eb5bcdad458431a12b7f03abc34f6d
Anton Simeonov <djnoreaga@gmail.com>
2017-12-06 15:59:41 +0200

    Work on Harry Poter 4D

---
+++ Exam_1_CSharp_Advanced/HarryPoter_4D/HarryPotter_4D.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarryPoter_4D
+{
+    class HarryPotter_4D
+    {
+        static void Main()
+        {
+            int[] dimensions = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int a = dimensions[0];
+            int b = dimensions[1];
+            int c = dimensions[2];
+            int d = dimensions[3];
+
+            string[,,,] cubeLike = new string[a, b, c, d];
+            //Read HP Coordinates
+            int[] hpCoord = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            cubeLike[hpCoord[0], hpCoord[1], hpCoord[2], hpCoord[3]] = "@";
+
+            List<string[]> basiilisks = new List<string[]>();
+            int s = int.Parse(Console.ReadLine());
+            for (int i = 0; i < s; i++)
+            {
+                // Input of basilisks names and positions
+                string[] inputBasilisk = Console.ReadLine().Split(' ').ToArray();
+                //Keep record of basilisk's name and positions
+                basiilisks[i] = inputBasilisk;
+                cubeLike[int.Parse(inputBasilisk[1]), int.Parse(inputBasilisk[2]),
+                    int.Parse(inputBasilisk[3]), int.Parse(inputBasilisk[4])] = inputBasilisk[0];
+            }
+
+            while (true)
+            {
+                string[] command = Console.ReadLine().Split(' ').ToArray();
+                int deltaCurrCoord = int.Parse(command[2]);
+                foreach (var item in basiilisks)
+                {
+                    if (command[0] == item[0])
+                    {
+                        item = command;
+                    }
+                }
+
+            }
+
+        }
+    }
+}
