cf2fe6fe615669344041fd0e36227b46c9f31762
simeonovanton <djnoreaga@gmail.com>
2017-12-10 11:13:37 +0200

    practice

---
+++ 04.OOP/Practice_01_OOP/Proba_2/Namespace/Namespace.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+//namespace Outer.Inner
+namespace SomeNamespace
+{
+    public class MyClass
+    {
+        public static double GetPiValue()
+        {
+            // This will not compile because
+            // the compiler searches first the Outer namespace
+            // and there is our Math class which
+            // does not have PI property
+            return Math.PI;
+        }
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var pi = MyClass.GetPiValue();
+            Console.WriteLine(pi);
+        }
+    }
+}
+
+namespace Outer.NewInner
+{
+    using System;
+
+    public class MyClass
+    {
+        public static double GetPiValue()
+        {
+            // Here the copiler searcher first System namespace
+            // and founds the PI in the System.Math class
+            return Math.PI;
+        }
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var pi = MyClass.GetPiValue();
+        }
+    }
+}
+
+namespace Outer
+{
+    // My math class
+    class Math
+    {
+    }
+}
+
