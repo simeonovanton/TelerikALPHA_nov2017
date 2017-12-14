916d711f5975d508c4ab5d1955f268c51b29be40
simeonovanton <djnoreaga@gmail.com>
2017-12-14 22:09:02 +0200

    Some OOP principles practice override vs. new

---
+++ 04.OOP/Practice_01_OOP/Proba_2/DerrivedAndBaseClass/DerivedClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DerrivedAndBaseClass
+{
+    class DerivedClass : BaseClass
+    {
+        public override void Method1()
+        {
+            Console.WriteLine("Derived - Method1");
+        }
+
+        public new void Method2()
+        {
+            Console.WriteLine("Derived - Method2");
+        }
+    }
+}
---
+++ 04.OOP/Practice_01_OOP/Proba_2/DerrivedAndBaseClass/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DerrivedAndBaseClass
+{
+    class Program
+    {
+        static void Main()
+        {
+            BaseClass bc = new BaseClass();
+            DerivedClass dc = new DerivedClass();
+            BaseClass bcdc = new DerivedClass();
+
+            bc.Method1();
+            bc.Method2();
+            dc.Method1();
+            dc.Method2();
+            bcdc.Method1();
+            bcdc.Method2();
+
+            Console.WriteLine(new string('=', 20));
+
+            
+        }
+    }
+}
