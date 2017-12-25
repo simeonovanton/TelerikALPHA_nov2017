01f37204319cab694ad2317caab18fea53f1bdc3
simeonovanton <djnoreaga@gmail.com>
2017-12-25 15:42:01 +0200

    Extension methods practice

---
+++ 04.OOP/ExtensionDelegateLinqLamda/ExtMethLinqLambda/ExtensionMethods/ExtMethod1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    public static class Extensions
+    {
+        public static void IncreaseWith(this IList<int> list, int amount)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                list[i] += amount;
+            }
+        }
+    }
+}
---
+++ 04.OOP/ExtensionDelegateLinqLamda/ExtMethLinqLambda/ExtensionMethods/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<int> integers = new List<int> { 1, 2, 3, 4, 5 };
+            integers.IncreaseWith(5);
+            Console.WriteLine(string.Join(", ", integers));
+        }
+    }
+}
