ae5abc26c886e10c7d2f17c4236f2bb323b268f2
Anton Simeonov <djnoreaga@gmail.com>
2018-01-12 11:00:39 +0200

    Cuki Skoki

---
+++ 05.DSA/WorkShop_1/Start_Up/Start_Up/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Start_Up
+{
+    class Program
+    {
+        static void Main()
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            var buildings = Console.ReadLine()
+
+                .Split().Select(x => int.Parse(x)).ToArray();
+
+            Stack<int> jumpsPerBuilding = new Stack<int>();
+            int[] maxJumpsMatrix = new int[N];
+            int maxJump = 0;
+            for (int i = 0; i < N - 1; i++)
+            {
+                int maxJumps = 0;
+                for (int j = i + 1; j < N; j++)
+                {
+                    if (buildings[i] < buildings[j] )
+                    {
+                        jumpsPerBuilding.Push(buildings[j]);
+                        if (jumpsPerBuilding.Count >= maxJumps)
+                        {
+                            maxJumps = jumpsPerBuilding.Count;
+                        }
+                    }
+                    else
+                    {
+                        jumpsPerBuilding.Clear();
+                    }
+                }
+                maxJumpsMatrix[i] = maxJumps;
+            }
+
+            foreach (var jump in maxJumpsMatrix)
+            {
+                if (jump >= maxJump)
+                {
+                    maxJump = jump;
+                }
+            }
+
+            Console.WriteLine(maxJump);
+            Console.WriteLine(string.Join(" ", maxJumpsMatrix));
+        }   
+    }
+}
