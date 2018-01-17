9cd7a79def9cb1b9c61ea207eec1276318859bb1
simeonovanton <djnoreaga@gmail.com>
2018-01-17 22:19:02 +0200

    Power Collections Documentation added and some practice with PowerCollections

---
+++ 05.DSA/LinearDataStructures/LinearStructures/OredredSetTest/OrderedSetTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wintellect.PowerCollections;
+
+namespace OredredSetTest
+{
+    class OrderedSetTest
+    {
+        static void Main()
+        {
+            OrderedSet<int> set = new OrderedSet<int>(new int[] {5, 2, 1, 9, 7, 4, 3});
+            foreach (var value in set)
+            {
+                Console.Write($"{value} ");
+            }
+            Console.WriteLine();
+            Console.WriteLine(set.Range(3, true, 6, true));
+            IEnumerable<int> list = set.Range(3, true, 6, true);
+            foreach (var value in list)
+            {
+                Console.Write($"{value} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
