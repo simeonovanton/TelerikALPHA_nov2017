4375f3e5dd8a2ff46d33413a94edd4a431cbf5f6
Anton Simeonov <djnoreaga@gmail.com>
2018-01-22 11:16:55 +0200

    secret message 0 test

---
+++ 05.DSA/WorkShop_1/Start_Up/Exam1/SecretMessage.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam1
+{
+    class SecretMessage
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            Stack<int> brackets = new Stack<int>();
+            Stack<int> digits = new Stack<int>();
+            int multiplyDigit = 1;
+
+            string input = Console.ReadLine();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '{')
+                {
+                    brackets.Push(i);
+                }
+                else
+                {
+                        if (input[i] == '}')
+                        {
+                            int startIndex = brackets.Pop();
+
+                            if (input[startIndex - 1] != -1 && Char.IsDigit(input[startIndex - 1]))
+                            {
+                                multiplyDigit = input[startIndex - 1] - '0';
+                            }
+
+                            for (int j = 0; j < multiplyDigit; j++)
+                            {
+                                sb.Append(input.Substring(startIndex + 1, i - startIndex - 1));
+                            }
+                            
+                            //if (brackets.Any())
+                            //{
+                            //    sb.Append(" | ");
+                            //}
+                        }
+                    //input = sb.ToString();
+                }
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
