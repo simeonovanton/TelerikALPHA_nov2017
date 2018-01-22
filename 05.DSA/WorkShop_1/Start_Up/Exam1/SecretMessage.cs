c9d2033da6edc48523b456b04b307881e72d0792
Anton Simeonov <djnoreaga@gmail.com>
2018-01-22 20:09:57 +0200

    Exam

--- 05.DSA/WorkShop_1/Start_Up/Exam1/SecretMessage.cs
+++ 05.DSA/WorkShop_1/Start_Up/Exam1/SecretMessage.cs
@@ -11,43 +11,80 @@ namespace Exam1
         static void Main(string[] args)
         {
             StringBuilder sb = new StringBuilder();
+            StringBuilder head = new StringBuilder();
+            StringBuilder tail = new StringBuilder();
             Stack<int> brackets = new Stack<int>();
             Stack<int> digits = new Stack<int>();
             int multiplyDigit = 1;
+            int multiplyNumber = 1;
 
             string input = Console.ReadLine();
-
-            for (int i = 0; i < input.Length; i++)
+           
+            while(!(input.IndexOf('{') != -1) )
             {
-                if (input[i] == '{')
+                for (int i = 0; i < input.Length; i++)
                 {
-                    brackets.Push(i);
+                    if (input[i] == '{' || Char.IsDigit(input[i]))
+                    {
+                        head.Append(input.Substring(0, i));
+                        break;
+                    }
+
                 }
-                else
+                //Console.WriteLine(head.ToString());
+                for (int i = input.Length - 1; i > -1; i--)
                 {
+                    if (input[i] == '}' && i != input.Length - 1)
+                    {
+                        tail.Append(input.Substring(i + 1, input.Length - 1 - i));
+                        break;
+                    }
+                }
+                //Console.WriteLine(tail.ToString());
+
+                for (int i = 0; i < input.Length; i++)
+                {
+                    if (input[i] == '{')
+                    {
+                        brackets.Push(i);
+                    }
+                    else
+                    {
                         if (input[i] == '}')
                         {
                             int startIndex = brackets.Pop();
 
                             if (input[startIndex - 1] != -1 && Char.IsDigit(input[startIndex - 1]))
                             {
-                                multiplyDigit = input[startIndex - 1] - '0';
+                                multiplyNumber = 0;
+                                for (int d = startIndex - 1; d > -1; d--)
+                                {
+                                    if (Char.IsDigit(input[d]))
+                                    {
+                                        multiplyNumber = multiplyDigit = input[d] - '0';
+                                        if (d - 1 != -1)
+                                        {
+                                            multiplyNumber = multiplyNumber + multiplyDigit * (int)Math.Pow(10, startIndex - 1 - d);
+                                        }
+                                    }
+                                }
+                                //multiplyDigit = input[startIndex - 1] - '0';
                             }
 
-                            for (int j = 0; j < multiplyDigit; j++)
+                            for (int j = 0; j < multiplyNumber; j++)
                             {
                                 sb.Append(input.Substring(startIndex + 1, i - startIndex - 1));
                             }
-                            
-                            //if (brackets.Any())
-                            //{
-                            //    sb.Append(" | ");
-                            //}
                         }
-                    //input = sb.ToString();
+                        //input = sb.ToString();
+                    }
                 }
+               
+                input = sb.ToString();
+                input = head.ToString() + input + tail.ToString();
             }
-            Console.WriteLine(sb.ToString());
+            
+            Console.WriteLine(input);
         }
     }
 }
