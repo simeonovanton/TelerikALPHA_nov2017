a1dc87e235d5992b82948125ffd881905a01fd68
simeonovanton <djnoreaga@gmail.com>
2018-01-29 05:09:25 +0200

    UnitsOfWork Done 100%

---
+++ 05.DSA/WorkShop_1/Start_Up/Exam3/Expressions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam3
+{
+    class Expressions
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int number = int.Parse(Console.ReadLine());
+            int n = input.Length;
+            int[] digits = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                digits[i] = input[i] - '0';
+            }
+        }
+    }
+}
--- 05.DSA/WorkShop_1/Start_Up/UnitsOfWork/UnitsOfWork.cs
+++ 05.DSA/WorkShop_1/Start_Up/UnitsOfWork/UnitsOfWork.cs
@@ -7,13 +7,15 @@ using Wintellect.PowerCollections;
 
 namespace UnitsOfWork
 {
-
-    class UnitsOfWork
+    public class UnitsOfWork
     {
-        private static SortedDictionary<string, Unit> dictionary = new SortedDictionary<string, Unit>();
-        private static string[] command;
+        public static Dictionary<string, Unit> dictionaryOfUnits = new Dictionary<string, Unit>();
+        public static Dictionary<string, SortedSet<Unit>> unitByType = new Dictionary<string, SortedSet<Unit>>();
+        public static SortedSet<Unit> unitByAttack = new SortedSet<Unit>();
+        public static string[] command;
+        public static StringBuilder resultBuilder = new StringBuilder();
 
-        private class Unit
+        public class Unit : IComparable<Unit>
         {
             public Unit(string name, string type, int attack)
             {
@@ -21,37 +23,105 @@ namespace UnitsOfWork
                 this.Type = type;
                 this.Attack = attack;
             }
-            public string Name  { get; set; }
-            public string Type  { get; set; }
-            public int Attack  { get; set; }
+            public string Name { get; set; }
+            public string Type { get; set; }
+            public int Attack { get; set; }
+
+            public int CompareTo(Unit other)
+            {
+                if (this.Attack == other.Attack)
+                {
+                    return this.Name.CompareTo(other.Name);
+                }
+                else
+                {
+                    return this.Attack.CompareTo(other.Attack) * -1;
+                }
+            }
+
+            public override string ToString()
+            {
+                return $"{this.Name}[{this.Type}]({this.Attack})";
+            }
         }
 
         static void Main()
         {
-            command = Console.ReadLine().Trim().Split().ToArray();
+            string commands;
 
-            while (command[0] != "end")
+            while ((commands = Console.ReadLine()) != "end")
             {
+                string[] command = commands.Trim().Split().ToArray();
                 switch (command[0])
                 {
                     case "add":
-                        break;
+                        AddUnit(command); break;
                     case "remove":
-                        break;
+                        RemoveUnit(command); break;
                     case "find":
-                        break;
+                        FindByType(command); break;
                     case "power":
-                        break;
+                        Power(command); break;
                     default:
                         break;
                 }
             }
-           
+            Console.WriteLine(resultBuilder.ToString());
         }
 
-        private static void AddUnit()
+        public static void AddUnit(string[] command)
         {
-            Unit unitToAdd = new Unit(command[1], command[2], int.Parse(command[3]));
+            var unitToAdd = new Unit(command[1], command[2], int.Parse(command[3]));
+            if (dictionaryOfUnits.ContainsKey(unitToAdd.Name))
+            {
+                resultBuilder.AppendLine($"FAIL: {unitToAdd.Name} already exists!");
+            }
+            else
+            {
+                if (!unitByType.ContainsKey(command[2]))
+                {
+                    unitByType.Add(command[2], new SortedSet<Unit>());
+                }
+                unitByType[command[2]].Add(unitToAdd);
+                dictionaryOfUnits.Add(unitToAdd.Name, unitToAdd);
+                unitByAttack.Add(unitToAdd);
+                resultBuilder.AppendLine($"SUCCESS: {unitToAdd.Name} added!");
+            }
+        }
+
+            public static void RemoveUnit(string[] command)
+            {
+                if (dictionaryOfUnits.ContainsKey(command[1]))
+                {
+                    Unit unitToRemove = dictionaryOfUnits[command[1]];
+                    unitByType[unitToRemove.Type].Remove(unitToRemove);
+                    unitByAttack.Remove(unitToRemove);
+                    dictionaryOfUnits.Remove(command[1]);
+                    resultBuilder.AppendLine($"SUCCESS: {command[1]} removed!");
+                }
+                else
+                {
+                    resultBuilder.AppendLine($"FAIL: {command[1]} could not be found!");
+                }
+            }
+
+            public static void FindByType(string[] command)
+            {
+                if (unitByType.ContainsKey(command[1]))
+                {
+                    resultBuilder.AppendLine(string.Format("RESULT: {0}", string.Join(", ", unitByType[command[1]].Take(10))));
+                }
+                else
+                {
+                    resultBuilder.AppendLine("RESULT: ");
+                }
+
+            }
+
+            public static void Power(string[] command)
+            {
+                int number = int.Parse(command[1]);
+                resultBuilder.AppendLine(string.Format("RESULT: {0}", string.Join(", ", unitByAttack.Take(number))));
+            }
         }
     }
-}
