49e7261be81824f69e4486fc64528cc5bc2772a6
Anton Simeonov <djnoreaga@gmail.com>
2018-02-13 20:18:33 +0200

    prepare for dsa2

---
+++ 05.DSA/DSA2prepare/Portals/Portals/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portals
+{
+    class Program
+    {
+        public static int R;
+        public static int C;
+        public static int startR;
+        public static int startC;
+        public static char[,] labirynth;
+
+        static void Main()
+        {
+
+        }
+    }
+}
