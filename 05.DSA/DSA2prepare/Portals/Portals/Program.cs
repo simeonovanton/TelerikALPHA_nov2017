01685545e28d66dffe373112be15dc7a839b0f37
simeonovanton <djnoreaga@gmail.com>
2018-02-13 22:18:28 +0200

    dsa

--- 05.DSA/DSA2prepare/Portals/Portals/Program.cs
+++ 05.DSA/DSA2prepare/Portals/Portals/Program.cs
@@ -16,7 +16,21 @@ namespace Portals
 
         static void Main()
         {
-
+            int[] xy = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            startR = xy[0];
+            startC = xy[1];
+            int[] rc = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            R = rc[0];
+            C = rc[1];
+            labirynth = new char[R, C];
+            for (int i = 0; i < R; i++)
+            {
+                char[] line = Console.ReadLine().Split(' ');
+                for (int j = 0; j < C; j++)
+                {
+                    labirynth[i, j] = line[j];
+                }
+            }
         }
     }
 }
