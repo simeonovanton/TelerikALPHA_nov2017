f81c495236b960236e952f8fc0497b75b5a6dac5
simeonovanton <djnoreaga@gmail.com>
2018-04-19 23:49:20 +0300

    some JSON tests

---
+++ 09.ASP.NET_MVC/JSON_Newtonsoft_Tests/JSONtests/JSONtests/Product.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JSONtests
+{
+    public class Product
+    {
+        public Product()
+        {
+
+        }
+
+        public Product(string name, decimal price, DateTime expiresAt, string size)
+        {
+            this.Price = price;
+            this.ExpiresAt = expiresAt;
+            this.Size = size;
+            this.Name = name;
+        }
+
+        [JsonProperty(PropertyName = "Name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "Price")]
+        public decimal Price { get; set; }
+
+        [JsonProperty(PropertyName = "ExpiresAt")]
+        public DateTime ExpiresAt { get; set; }
+
+        [JsonProperty(PropertyName = "Size")]
+        public string Size { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Name: {this.Name}");
+            sb.AppendLine($"Price: {this.Price}");
+            sb.AppendLine($"ExpiresAt: {this.ExpiresAt}");
+            sb.AppendLine($"Size: {this.Size}");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ 09.ASP.NET_MVC/JSON_Newtonsoft_Tests/JSONtests/JSONtests/Program.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace JSONtests
+{
+    class Program
+    {
+        static void Main()
+        {
+            Product apple = new Product("Apple", 0.11M, new DateTime(2018,12,31), "Small");
+            string outputJSON = JsonConvert.SerializeObject(apple);
+
+            Console.WriteLine(outputJSON);
+
+            // Product newObject = JsonConvert.DeserializeObject<Product>(outputJSON);
+            Product newObject = JsonConvert.DeserializeObject<Product>(outputJSON);
+            Console.WriteLine(newObject);
+        }
+    }
+}
